b0f5f002a6de5de2b5231d5a984cc1eb66e47622
MonojitD <monojitdeb66@gmail.com>
2023-07-25 17:26:05 +0530

    Adding files to git

---
+++ src/views/Home/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './HomePage.css'
+
+const HomePage = () => {
+  return (
+    <>
+    <section className="home-hero">
+        <div className="left-area">
+            <h1 className="text-purple-600">This is home page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere hic non nulla, reprehenderit et velit. Hic, odit. Dolor, doloribus hic.</p>
+            <a href="/" target="_blank" rel="noopener noreferrer">Link</a>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default HomePage
